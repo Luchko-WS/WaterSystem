7027bf73688dd01b6d358d53eacadaa4ffb92c76
Luchko-WS <andriyluchko@gmail.com>
2019-03-09 00:22:37 +0200

    angularjs

--- OpenDataStorage/App_Start/BundleConfig.cs
+++ OpenDataStorage/App_Start/BundleConfig.cs
@@ -1,18 +1,20 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace OpenDataStorage
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularjs")
+                .Include("~/Scripts/angularjs/angular.js")
+                .IncludeDirectory("~/Scripts/angularjs/angular-ui", "*.js", true));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -20,7 +22,7 @@ namespace OpenDataStorage
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
