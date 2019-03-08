b61608ae789befe2f01694ebc8804ac9de4319a8
Luchko-WS <andriyluchko@gmail.com>
2019-03-09 00:52:12 +0200

    ng-app

--- OpenDataStorage/App_Start/BundleConfig.cs
+++ OpenDataStorage/App_Start/BundleConfig.cs
@@ -7,26 +7,29 @@ namespace OpenDataStorage
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery/jquery-{version}.js"));
+                        "~/Scripts/libs/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angularjs")
-                .Include("~/Scripts/angularjs/angular.js")
-                .IncludeDirectory("~/Scripts/angularjs/angular-ui", "*.js", true));
+                .Include("~/Scripts/libs/angularjs/angular.js")
+                .IncludeDirectory("~/Scripts/libs/angularjs/angular-ui", "*.js", true));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery/jquery.validate*"));
+                        "~/Scripts/libs/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/libs/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap/bootstrap.js"));
+                      "~/Scripts/libs/bootstrap/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/app/app.js"));
         }
     }
 }
---
+++ OpenDataStorage/Scripts/app/app.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+    window.app = angular.module('MainApp',
+        [
+            'ui.bootstrap',
+            //'ngCookies',
+            //'pascalprecht.translate'
+        ]);
+
+    /*app.config(['$translateProvider', function ($translateProvider) {
+        $translateProvider.useUrlLoader('/api/Languages');
+        $translateProvider.useSanitizeValueStrategy('escapeParameters');
+
+        var $cookies;
+        angular.injector(['ngCookies']).invoke(['$cookies', function (_$cookies_) {
+            $cookies = _$cookies_;
+        }]);
+
+        var lang = $cookies.get('culture');
+        if (!lang) {
+            lang = 'en';
+        }
+        $translateProvider.preferredLanguage(lang);
+    }]);*/
+})();
