1416d6e6a3c07f97fdd701ee352b57410519fa13
Luchko-WS <andriyluchko@gmail.com>
2019-03-10 13:58:25 +0200

    selected list helper

---
+++ OpenDataStorage/Helpers/SelectedListHelper.cs
@@ -0,0 +1,43 @@
+﻿using OpenDataStorage.Common;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace OpenDataStorage.Helpers
+{
+    public static class SelectedListHelper
+    {
+        public static IEnumerable<SelectListItem> Countries
+        {
+            get
+            {
+                List<string> countries = new List<string>();
+                foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+                {
+                    var ri = new RegionInfo(ci.Name);
+                    countries.Add(ri.DisplayName);
+                }
+                countries = countries.Distinct().ToList();
+                countries.Sort();
+
+                return new SelectList(countries);
+            }
+        }
+
+        public static IEnumerable<SelectListItem> AvailableLanguages
+        {
+            get
+            {
+                string selectedLanguage = LanguageManager.GetDefaultLanguage();
+                string threadLanguage = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+                if (LanguageManager.AvailableLanguages.Exists(l => l.LangCultureName == threadLanguage))
+                {
+                    selectedLanguage = threadLanguage;
+                }
+                return new SelectList(LanguageManager.AvailableLanguages, "LangCultureName", "LangFullName", selectedLanguage);
+            }
+        }
+    }
+}
