f8fb53dd6c2ece7e650858a44844737b09a2f0b3
Luchko-WS <andriyluchko@gmail.com>
2019-05-12 10:17:53 +0300

    refactor bundles names

--- OpenDataStorage/App_Start/BundleConfig.cs
+++ OpenDataStorage/App_Start/BundleConfig.cs
@@ -6,11 +6,11 @@ namespace OpenDataStorage
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery")
                 .Include("~/Scripts/libs/jquery/jquery-{version}.js")
                 .Include("~/Scripts/libs/jquery/jquery-ui-1.12.1.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/angularjs")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/angularjs")
                 .Include("~/Scripts/libs/angularjs/angular.js")
                 .Include("~/Scripts/libs/angularjs/angular-cookies.js")
                 .Include("~/Scripts/libs/angularjs/angular-translate.js")
@@ -18,15 +18,15 @@ namespace OpenDataStorage
                 .Include("~/Scripts/libs/angularjs/angular-ui/ui-bootstrap.js")
                 .Include("~/Scripts/libs/angularjs/angular-ui/ui-bootstrap-tpls.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/jqueryval")
                 .Include("~/Scripts/libs/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/modernizr")
                 .Include("~/Scripts/libs/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/bootstrap")
                 .Include("~/Scripts/libs/bootstrap/bootstrap.js")
                 .Include("~/Scripts/libs/bootstrap/bootstrap-treeview.js"));
 
@@ -37,20 +37,20 @@ namespace OpenDataStorage
                       "~/Content/css/ui-bootstrap-csp.css",
                       "~/Content/css/site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/app")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/app")
                 .Include("~/Scripts/app/app.js")
                 .IncludeDirectory("~/Scripts/app/Common/Services", "*.js", true)
                 .IncludeDirectory("~/Scripts/app/Common/Controllers", "*.js", true)
                 .IncludeDirectory("~/Scripts/app/Common/Directives", "*.js", true)
                 .IncludeDirectory("~/Scripts/app/Common/Filters", "*.js", true));
 
-            bundles.Add(new ScriptBundle("~/bundles/app/Characteristic")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/app/Characteristic")
                 .IncludeDirectory("~/Scripts/app/Characteristic", "*.js", true));
 
-            bundles.Add(new ScriptBundle("~/bundles/app/HierarchyObject")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/app/HierarchyObject")
                 .IncludeDirectory("~/Scripts/app/HierarchyObject", "*.js", true));
 
-            bundles.Add(new ScriptBundle("~/bundles/app/ObjectType")
+            bundles.Add(new ScriptBundle("~/bundles/scripts/app/ObjectType")
                 .IncludeDirectory("~/Scripts/app/ObjectType", "*.js", true));
         }
     }
